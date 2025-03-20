b26530aee86889adc6fa989b8f66035808253f53
Stiven_Hendra <stiven.hendra26@gmail.com>
2025-03-20 16:22:56 +0700

    Final touch

--- resources/views/BuatTenant.blade.php
+++ resources/views/BuatTenant.blade.php
@@ -31,7 +31,7 @@
                                 <label for="email"
                                     class="block text-sm font-medium leading-6 text-gray-900">Email</label>
                                 <div class="mt-2">
-                                    <input required id="email" name="email" type="text"
+                                    <input required id="email" name="email" type="email"
                                         class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
                                 </div>
                             </div>
@@ -232,3 +232,11 @@
 
     </main>
 </x-layout>
+
+
+
+<script>
+    document.getElementById('id').addEventListener('input', function (e) {
+        this.value = this.value.replace(/ /g, '_');
+    });
+</script>
